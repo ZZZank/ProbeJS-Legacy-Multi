1391610ef6c275e0d9ac5cc6633f5deaa5aa3fd3
ZZZank <3410764033@qq.com>
2025-03-30 14:11:55 +0800

    declare two mods: probejs and probejs_legacy

---
+++ src/main/java/zzzank/probejs/ProbeJSLegacy.java
@@ -0,0 +1,11 @@
+package zzzank.probejs;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * @author ZZZank
+ */
+@Mod(ProbeJSLegacy.MOD_ID)
+public class ProbeJSLegacy {
+    public static final String MOD_ID = "probejs_legacy";
+}
