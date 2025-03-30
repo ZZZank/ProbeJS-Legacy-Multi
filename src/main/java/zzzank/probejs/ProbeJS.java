9ed111fb378cbf5a757798ee40d22b6c2fd4eea6
ZZZank <3410764033@qq.com>
2025-03-30 17:00:27 +0800

    register plugin by ourselves

--- src/main/java/zzzank/probejs/ProbeJS.java
+++ src/main/java/zzzank/probejs/ProbeJS.java
@@ -3,13 +3,18 @@ package zzzank.probejs;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import dev.architectury.platform.forge.EventBuses;
+import dev.latvian.mods.kubejs.KubeJSPlugin;
+import dev.latvian.mods.kubejs.util.KubeJSPlugins;
+import lombok.val;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import zzzank.probejs.plugin.BuiltinProbeJSPlugin;
 import zzzank.probejs.utils.JsonUtils;
 
 import java.nio.file.Path;
+import java.util.List;
 
 @Mod(ProbeJS.MOD_ID)
 public class ProbeJS {
@@ -29,6 +34,23 @@ public class ProbeJS {
     public ProbeJS() {
         EventBuses.registerModEventBus(ProbeJS.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
 
+        insertPlugin();
+
         ProbeConfig.refresh();
+    }
+
+    /**
+     * why not 'kubejs.plugins.txt' you might ask, well, ProbeJS Legacy declared two mods
+     * so TWO plugins will be registered. Stupid KubeJS
+     */
+    private static void insertPlugin() {
+        try {
+            val field = KubeJSPlugins.class.getDeclaredField("LIST");
+            field.setAccessible(true);
+            val got = (List<KubeJSPlugin>) field.get(null);
+            got.add(new BuiltinProbeJSPlugin());
+        } catch (Throwable e) {
+            ProbeJS.LOGGER.error("unable to insert KubeJS plugin", e);
+        }
     }
 }
